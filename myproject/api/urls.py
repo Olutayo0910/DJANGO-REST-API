79786d254aef34d2c3632fa211130115bd7ebc45
Olutayo0910 <olutayoogunlade0910@gmail.com>
2024-10-14 10:54:20 +0100

    added dockerfile

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductListCreate.as_view(), name='product-list-create'),
     path('products/<int:pk>', views.ProductRetrieveUpdateDestroy.as_view(), name='product-update'),
-    path('products-list/', views.ProductListView.as_view(), name='product-list'),
-    
+    path('products/list/', views.ProductListView.as_view(), name='product-list'),
+    path('products/search/', views.ProductList.as_view(), name='product-search'),
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -22,3 +23,16 @@ class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductList(APIView):
+    def get(self, request, *args, **kwargs):
+
+        name = request.query_params.get("name", "")
+
+        if name:
+            product_list = Product.objects.filter(name__icontains=name)
+        else:
+            product_list = Product.objects.all()
+
+        serializer = ProductSerializer(product_list, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
